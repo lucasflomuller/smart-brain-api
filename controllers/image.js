0dc4931672ee3cfc1d6166cc7552c1a2ae78a600
lucasflomuller <lucasflorianomuller@gmail.com>
2019-04-11 00:16:57 +0000

    add dotenv package

--- controllers/image.js
+++ controllers/image.js
@@ -1,8 +1,8 @@
 const Clarifai = require('clarifai');
 
-//You must add your own API key here from Clarifai.
+//You must add your own API key on .env file from Clarifai.
 const app = new Clarifai.App({
- apiKey: 'YOUR_API_KEY_HERE'
+ apiKey: process.env.API_KEY
 });
 
 const handleApiCall = (req, res) => {
